3a9c6c1b7f718b418a331b632d065c288062ae73
Mohamed Farouk <mohamedfarouk1994@gmail.com>
2026-02-20 14:25:10 +0200

    update schemas

--- backend/schemas.py
+++ backend/schemas.py
@@ -16,8 +16,6 @@ class UserCreate(UserLogin):
 class UserResponse(UserBase):
     id: int
     email: EmailStr
-    is_active: bool
-    is_superuser: bool
 
     class Config:
         from_attributes = True
